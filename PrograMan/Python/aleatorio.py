e9a55f862eab49e533468266a13929a41be7bf1b
DanielMelo <danielmelo276@gmail.com>
2020-01-22 18:01:48 -0500

    Modificaciones al archivo

--- PrograMan/Python/aleatorio.py
+++ PrograMan/Python/aleatorio.py
@@ -1,3 +1,5 @@
+import random
+
 def run():
     numberFound = False
     randomNumber = random.randint(0, 20)
@@ -5,8 +7,12 @@ def run():
     while not numberFound:
         number = int(input('Adivina el numero: '))
 
-    if number == randomNumber:
-        print('Felicidades!! Encontraste el numero')
-    elif 
+        if number == randomNumber:
+            print('Felicidades!! Encontraste el numero')
+            numberFound = True
+        elif number > randomNumber:
+            print('El numero es mas pequeño')
+        else:
+            print('El numero es mas grande')
 if __name__ == '__main__':
     run()
