176c9e417757c9a4f8bb02dca25463ae5324a4e3
DanielMelo <danielmelo276@gmail.com>
2020-01-22 13:05:47 -0500

    FizzBuzz en python

---
+++ PrograMan/Python/fizzbuzz.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    
+    print('F I Z Z B U Z Z  E N  P Y T H O N')
+    
+    for i in range (1, 100):   
+        if (i % 3 ==0):
+            print('Fizz')
+        if (i % 5 ==0):
+            print('Buzz')
+        if (i % 3 == 0 and i % 5 ==0):
+            print('FizzBuzz')    
+        if (i % 3 != 0 and i % 5 !=0):
+            print(i)
